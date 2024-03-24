a2c3302494d6cc614050083d636f4eb3b4e8e793
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-03-24 21:37:00 +0100

    Add Polish translations

---
+++ lang/pl/hub.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hub Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used on the hub page.
+    |
+    */
+
+    'choose-website' => 'Wybierz stronę, na którą chcesz przejść',
+    'welcome' => 'Witaj w Hubie <b>Kadziela.pl</b>',
+
+];
---
+++ lang/pl/main.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Main Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in the main layout.
+    |
+    */
+
+    'description' => 'Kadziela.pl - Hub dla wszystkich subdomen kadziela.pl',
+
+    'footer' => [
+        'programmed-with' => 'Zaprojektowane i zaprogramowane z',
+        'by' => 'przez',
+        'author' => 'Jonasza Kądzielę',
+    ],
+    'titles' => [
+        'contact' => 'Kontakt',
+        'hub' => 'Hub',
+    ],
+
+];
