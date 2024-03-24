f1d0fb75050aeb335df923026295df0c0f7925e5
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-03-24 23:10:39 +0100

    Add LanguageController that allows user to change locale

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\LanguageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('hub'))->name('hub');
+
+Route::get('/language/{code}', [LanguageController::class, 'change']);
