4f1f8d262fbe070939d8bd362ee7528dfbbe460a
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-05-03 18:55:17 +0200

    Add favicons, browserconfig.xml and manifest.json

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,12 +10,16 @@
         <meta name="keywords" content="kadziela, hub, website">
         <meta name="author" content="Jonasz Kądziela">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="theme-color" content="#374151">
+        <meta name="msapplication-TileColor" content="#374151">
         <meta property="og:locale" content="{{ App::getLocale() }}">
         <meta property="og:locale:alternate" content="{{ App::getFallbackLocale() }}">
         <meta property="og:site_name" content="{{ config('app.name') }}">
         <meta property="og:title" content="{{ (isset($title) ? $title . ' - ' : '') . config('app.name') }}">
         <meta property="og:description" content="{{ Lang::get('main.description') }}">
         <meta property="og:url" content="{{ config('app.url') }}">
+        <x-favicons />
+        <link rel="manifest" href="/manifest.json">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @if(App::isProduction() && isset($withAnalytics))
