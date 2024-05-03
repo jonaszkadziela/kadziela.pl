fc83a0b14effbec18bc51ade54214a418a3967ac
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-05-03 19:21:14 +0200

    Add loading spinner to website button

--- resources/views/components/website-button.blade.php
+++ resources/views/components/website-button.blade.php
@@ -1,7 +1,11 @@
-<a href="{{ $link }}" rel="noopener noreferrer">
+<a href="{{ $link }}"
+   rel="noopener noreferrer"
+   x-data="{ loading: false }"
+   @click="loading = true"
+>
     <div class="bg-gray-700 cursor-pointer duration-150 ease-in-out flex flex-col h-24 hover:-translate-y-1 hover:bg-gray-800 justify-center p-2 text-white transform-all w-36">
-        <i class="{{ $icon }} mb-1"></i>
-        <p class="font-light">
+        <i x-bind:class="loading ? 'fa-circle-notch fa-solid fa-spin text-4xl' : '{{ $icon }}'"></i>
+        <p class="font-light mt-1">
             {{ $name }}
         </p>
     </div>
