7028d378b160c78270a77da7f98dae9b42b5015f
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-05-04 15:03:23 +0200

    Fix issue with loading indicator being immediately shown when navigating back from subdomain

--- resources/views/components/website-button.blade.php
+++ resources/views/components/website-button.blade.php
@@ -2,6 +2,7 @@
    rel="noopener noreferrer"
    x-data="{ loading: false }"
    @click="loading = true"
+   @pageshow.window="loading = false"
 >
     <div class="bg-gray-700 cursor-pointer duration-150 ease-in-out flex flex-col h-24 hover:-translate-y-1 hover:bg-gray-800 justify-center p-2 text-white transform-all w-36">
         <i x-bind:class="loading ? 'fa-circle-notch fa-solid fa-spin text-4xl' : '{{ $icon }}'"></i>
