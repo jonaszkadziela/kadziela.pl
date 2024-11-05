bf447f98a4014842d5be5c197b1708a5760d424e
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-11-06 00:53:15 +0100

    Add point-waves-background.js to vite config file

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/cookies.js',
+                'resources/js/point-waves-background.js',
             ],
             refresh: true,
         }),
