d26b22177cfa525eee583ced0fb24f1e6f193eb3
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-11-05 22:22:51 +0100

    Change structure of hub page

--- resources/views/hub.blade.php
+++ resources/views/hub.blade.php
@@ -4,31 +4,29 @@
                with-language-dropdown
                with-footer
 >
-    <main class="flex flex-1 items-center justify-center my-12 w-full">
-        <div class="bg-white border hover:shadow-xl max-w-3xl md:p-12 mx-8 p-8 shadow-md text-center transition-shadow">
-            <div class="flex justify-center mb-4">
-                <img class="h-24 w-24"
-                     src="{{ Vite::asset('resources/images/brand/logo.svg') }}"
-                     alt="Logo">
-            </div>
-            <h1 class="mb-8 md:mb-12 md:text-4xl text-3xl">
+    <main class="flex flex-1 flex-col items-center justify-center my-12 w-full">
+        <div class="flex flex-col gap-4 items-center mb-8 md:mb-12 mx-8 text-center">
+            <img class="h-24 w-24"
+                src="{{ Vite::asset('resources/images/brand/logo.svg') }}"
+                alt="Logo">
+            <h1 class="text-white text-3xl md:text-5xl lg:text-6xl">
                 {!! Lang::get('hub.welcome') !!}!
             </h1>
-            <div class="bg-slate-100 border md:p-8 p-4">
-                <p class="mb-4 md:mb-8 md:text-2xl text-gray-700 text-lg">
-                    {{ Lang::get('hub.choose-website') }}
-                </p>
-                <div class="flex flex-col items-center justify-center md:flex-row gap-4">
-                    <x-website-button name="bartosz.kadziela.pl"
-                                      link="https://bartosz.kadziela.pl"
-                    />
-                    <x-website-button name="jonasz.kadziela.pl"
-                                      link="https://jonasz.kadziela.pl"
-                    />
-                    <x-website-button name="milosz.kadziela.pl"
-                                      link="https://milosz.kadziela.pl"
-                    />
-                </div>
+        </div>
+        <div class="bg-white border hover:shadow-xl max-w-3xl md:p-12 mx-8 p-8 shadow-md text-center transition-shadow">
+            <p class="mb-4 md:mb-8 md:text-2xl text-gray-700 text-lg">
+                {{ Lang::get('hub.choose-website') }}
+            </p>
+            <div class="flex flex-col items-center justify-center md:flex-row gap-4">
+                <x-website-button name="bartosz.kadziela.pl"
+                                  link="https://bartosz.kadziela.pl"
+                />
+                <x-website-button name="jonasz.kadziela.pl"
+                                  link="https://jonasz.kadziela.pl"
+                />
+                <x-website-button name="milosz.kadziela.pl"
+                                  link="https://milosz.kadziela.pl"
+                />
             </div>
         </div>
     </main>
