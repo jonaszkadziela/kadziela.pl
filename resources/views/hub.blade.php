7780cc1daa5e7fd94c7870c9775506937a935a08
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-11-05 22:03:34 +0100

    Improve point waves background code

--- resources/views/hub.blade.php
+++ resources/views/hub.blade.php
@@ -1,5 +1,5 @@
 <x-main-layout title="{{ Lang::get('main.titles.hub') }}"
-               body-class="flex flex-col min-h-screen"
+               body-class="bg-black flex flex-col min-h-screen"
                with-analytics
                with-language-dropdown
                with-footer
@@ -32,6 +32,6 @@
             </div>
         </div>
     </main>
-    <canvas id="c" class="absolute inset-0" style="z-index: -1;"></canvas>
+    <canvas id="point-waves-background" class="absolute inset-0" style="z-index: -1;"></canvas>
     @vite('resources/js/point-waves-background.js')
 </x-main-layout>
