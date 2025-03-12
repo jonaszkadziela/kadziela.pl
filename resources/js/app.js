362838d982b72581cdef03da4959634bbfb8c35f
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-03-12 07:54:30 +0100

    Add dark or light class via JavaScript if when it is not present on html element

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,10 @@ import.meta.glob([
 window.Alpine = Alpine
 
 Alpine.start()
+
+const html = document.body.parentNode
+
+if (!html.classList.contains('light') && !html.classList.contains('dark')) {
+    html.classList.toggle('dark', window.matchMedia('(prefers-color-scheme: dark)').matches)
+    html.classList.toggle('light', window.matchMedia('(prefers-color-scheme: light)').matches)
+}
